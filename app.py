db65e04c6c9b5fbcaf95819d97152ef4e0835b8f
jaehwanchoi <79303416+lieblichoi@users.noreply.github.com>
2021-09-16 14:26:47 +0900

    변경 - app.py/login.html 충돌 코드 수정

--- app.py
+++ app.py
@@ -135,11 +135,9 @@ def sign_up():
     doc = {
         "username": username_receive,                               # 아이디
         "password": password_hash,                                  # 비밀번호
-        "profile_name": username_receive,                           # 프로필 이름 기본값은 아이디
         "nickname": nickname_receive,                               # 닉네임
         "profile_pic": "",                                          # 프로필 사진 파일 이름
         "profile_pic_real": "profile_pics/profile_placeholder.png", # 프로필 사진 기본 이미지
-        "profile_info": "",                                          # 프로필 한 마디
         "profile_info": profile_info_receive,  # 프로필 소개글
         "email": email_receive  # 이메일
     }
